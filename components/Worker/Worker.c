40c4e4192c477d14cdeeda98338ce97e0760d4fe
Felix Schladt <felix.schladt@hensoldt.net>
2024-02-23 10:48:53 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- components/Worker/Worker.c
+++ components/Worker/Worker.c
@@ -1,8 +1,11 @@
 /*
- *  Worker component that creates additional load for the system
+ * Worker component that creates additional load for the system
  *
- *  Copyright (C) 2019, HENSOLDT Cyber GmbH
+ * Copyright (C) 2019-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #include "lib_debug/Debug.h"
--- system_config.h
+++ system_config.h
@@ -1,9 +1,13 @@
 /**
- * Copyright (C) 2019, HENSOLDT Cyber GmbH
- *
  * OS libraries configurations
  *
+ * Copyright (C) 2019-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
+
 #pragma once
 
 //-----------------------------------------------------------------------------
